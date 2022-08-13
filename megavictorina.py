ac71241ce7a3a68eb706c1a0c18a2407c7c7d19a
polopeepka <106977777+polopeepka@users.noreply.github.com>
2022-08-14 00:41:02 +0600

    version 2

--- megavictorina.py
+++ megavictorina.py
@@ -13,15 +13,41 @@ def baby_questions_one(ask_age):
 
 def baby_questions_two(ask_age):
     while True:
-        ask_one_baby_two = input('Ты хорошо себя ведешь? - ')
-        if ask_one_baby_two == 'да':
+        ask_two_baby = input('Ты хорошо себя ведешь? - ')
+        if ask_two_baby == 'да':
             print('Молодец,так держать!')
             break
-        elif ask_one_baby_two == 'нет':
+        elif ask_two_baby == 'нет':
             print('Я думаю тебе стоит подумать над своим поведением и исправиться ')
             break
         else:
             print('Ты можешь отвечать только "да или нет"')
+
+def baby_questions_three(ask_age):
+    while True:
+        ask_three_baby = input('Ты любишь гулять на улице? -  - ')
+        if ask_three_baby == 'да':
+            print('Свежий воздух-это хорошо!')
+            break
+        elif ask_three_baby == 'нет':
+            print('Гулять-весело,но если тебе не нравиться,то можно посидеть дома еще чуток)')
+            break
+        else:
+            print('Ты можешь отвечать только "да или нет"')
+            continue
+
+def baby_questions_four(ask_age):
+    while True:
+        ask_four_baby = input('Тебе нравиться слушать музыку? - '  )
+        if ask_four_baby == 'да':
+            print('Я тоже люблю слушать музыку!')
+            break
+        elif ask_four_baby== 'нет':
+            print('Это твое дело,вот я например слушаю музыку,потому что она меня успокаивает.')
+            break
+        else:
+            print('Ты можешь отвечать только "да или нет"')
+            continue
 
 print('Эта программа будет задавать вопросы а которые вам нужно ответить,\n'
       'все вопросы будут заданы в зависимости от возраста того человека который ввел ссвой возраст в программе ')
@@ -29,7 +55,36 @@ print('Отвечай "да или нет") ')
 while True:
     ask_age = int(input('Сколько вам лет? - '))
     if ask_age <= 5:
-        baby_questions_one(ask_age)
-        baby_questions_two(ask_age)
+        duw = input(f"Оу,тебе всего {ask_age}\nты хочешь узнать первый вопрос? - ")
+        if duw == 'да':
+            print('Отлично!')
+            print('Я задам тебе пару вопросов для твоего возраста')
+            baby_questions_one(ask_age)
+            print('')
+            baby_questions_four(ask_age)
+        elif duw == 'нет':
+            print('эх,я думал мы подружимся,пока!')
+            break
+        else:
+            print('Ты можешь отвечать только "да или нет"')
+            continue
+
+
+        dul = input('Тебе понравилось? - ')
+        if dul.lower() == 'да':
+            print('Отлично,давай продолжим!')
+            print('')
+            baby_questions_three(ask_age)
+
+        elif dul.lower() == 'нет':
+            print('Эх, но у меня есть еще несколько вопросов,посмотри и на них,может тебе понравятся они')
+            print('')
+            baby_questions_two(ask_age)
+        else:
+            print('Ты можешь отвечать только "да или нет"')
+            continue
+
+        #baby_questions_two(ask_age)
+        #baby_questions_three(ask_age)
 
 
